e740a4fe886f7d0790eb8311913754a8072aac61
asil-creator2 <ic.asil2011@gmail.com>
2026-02-08 11:29:43 +0400

    seconf

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,6 @@
 import ContactBanner from "../Components/Contact/ContactBanner"
 import ContactUs from "../Components/Contact/ContactUs"
-import Location from "../Components/Contact/location"
+import Location from "../Components/Contact/Location"
 
 const Contact = () => {
   return (
